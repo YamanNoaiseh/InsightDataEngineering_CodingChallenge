70bf6ebf6e57c671e16b7981996485e5125a41a6
Yaman Noaiseh <yanoaiseh@gmail.com>
2016-07-15 22:56:03 -0400

    Shell Script

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-javac -cp ./lib/javax.json-1.0.jar:./lib/javax.json-api-1.0.jar ./src/VenmoGraph.java
-java -cp ./lib/javax.json-1.0.jar:./lib/javax.json-api-1.0.jar:./src VenmoGraph
+cd src
+javac -cp '../lib/javax.json-1.0.jar:../lib/javax.json-api-1.0.jar' VenmoGraph.java
+java -cp '.:../lib/javax.json-1.0.jar:../lib/javax.json-api-1.0.jar' VenmoGraph
